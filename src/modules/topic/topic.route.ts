bd81c7b61064ee7308c3abed8947e43ec9772b41
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-05-09 07:31:35 +0700

    feat: find topic

--- src/modules/topic/topic.route.ts
+++ src/modules/topic/topic.route.ts
@@ -2,14 +2,17 @@ import Container from 'typedi';
 import { createRemoveUndefinedValuesMiddleware } from '../../common/app/middlewares/remove-undefined-values.middleware';
 import {
   createBodyValidator,
+  createParamsValidator,
   createQueryValidator,
 } from '../../common/app/middlewares/request.middleware';
 import { createTrimBodyMiddleware } from '../../common/app/middlewares/trim-body.middleware';
 import { createJsonResponse } from '../../common/app/response';
 import { createRoute } from '../../common/app/router';
 import { CreateTopicDto } from './dto/create-topic.dto';
+import { FindTopicDto } from './dto/find-topic.dto';
 import { GetTopicDto } from './dto/get-topic.dto';
 import { TopicController } from './topic.controller';
+import { toNumber } from '../../common/helpers/convert-type.helper';
 
 export const topicRoute = createRoute<TopicController>(
   Container.get(TopicController)
@@ -23,5 +26,13 @@ export const topicRoute = createRoute<TopicController>(
     createQueryValidator(GetTopicDto),
     createRemoveUndefinedValuesMiddleware(),
     createJsonResponse(controller.getTopics),
+  ])
+  .get('/api/topics/:id', (controller: TopicController) => [
+    createParamsValidator(FindTopicDto, {
+      pre: (param) => ({
+        id: toNumber(param.id),
+      }),
+    }),
+    createJsonResponse(controller.findTopic),
   ])
   .getRouter();
