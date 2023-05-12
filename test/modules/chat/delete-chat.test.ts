68561517e255c66cb9fe857bce5116ec1d148cb2
ibra <ibrahimalanshor6@gmail.com>
2023-05-12 17:22:51 +0800

    feat: delete chat

--- test/modules/chat/delete-chat.test.ts
+++ test/modules/chat/delete-chat.test.ts
@@ -2,15 +2,17 @@ import { describe, it, before, beforeEach } from 'mocha';
 import supertest from 'supertest';
 import { expect } from '../../../src/common/test/chai';
 import { createObjectTransformerWithValidator } from '../../../src/common/dto/transform-and-validate-object';
-import { DeleteTopicParamsDto } from '../../../src/modules/topic/dto/delete-topic.dto';
 import { server } from '../../../server';
 import Container from 'typedi';
-import { TopicService } from '../../../src/modules/topic/topic.service';
 import { TopicRepository } from '../../../src/modules/topic/topic.repository';
-import { StoredTopic } from '../../../src/modules/topic/topic.entity';
+import { ChatService } from '../../../src/modules/chat/chat.service';
+import { ChatRepository } from '../../../src/modules/chat/chat.repository';
+import { DeleteChatParamsDto } from '../../../src/modules/chat/dto/delete-chat.dto';
+import { StoredChat } from '../../../src/modules/chat/chat.entity';
 
-describe.skip('delete topic test', () => {
-  const topicService = Container.get(TopicService);
+describe.only('delete chat test', () => {
+  const chatService = Container.get(ChatService);
+  const chatRepo = Container.get(ChatRepository);
   const topicRepo = Container.get(TopicRepository);
 
   describe('invalid param test', () => {
@@ -19,13 +21,13 @@ describe.skip('delete topic test', () => {
     };
     it('should throw validation schema error when param is invalid', async () => {
       const transformParam =
-        createObjectTransformerWithValidator(DeleteTopicParamsDto);
+        createObjectTransformerWithValidator(DeleteChatParamsDto);
 
       await expect(transformParam(invalidParams)).to.eventually.be.rejected;
     });
     it('should return 404 param is invalid', async () => {
       await supertest(server.httpServer)
-        .delete(`/api/topics/${invalidParams.id}`)
+        .delete(`/api/chats/${invalidParams.id}`)
         .expect(404);
     });
   });
@@ -34,37 +36,41 @@ describe.skip('delete topic test', () => {
       id: 999,
     };
     it('should throw empty result error when param is not found', async () => {
-      await expect(topicService.delete(params)).to.eventually.be.rejected;
+      await expect(chatService.delete(params)).to.eventually.be.rejected;
     });
     it('should return 404 when param is not found', async () => {
       await supertest(server.httpServer)
-        .delete(`/api/topics/${params.id}`)
+        .delete(`/api/chats/${params.id}`)
         .expect(404);
     });
   });
   describe('delete test', () => {
-    const params: { topic?: StoredTopic } = {};
+    const params: { chat?: StoredChat } = {};
 
     before(async () => {
+      await chatRepo.delete();
       await topicRepo.delete();
     });
 
     beforeEach(async () => {
-      params.topic = await topicService.create({
-        name: 'test',
+      const topic = await topicRepo.create({ name: 'test' });
+
+      params.chat = await chatService.create({
+        content: 'test',
+        topic_id: topic.id,
       });
     });
 
-    it('should delete topic by id', async () => {
+    it('should delete chat by id', async () => {
       await expect(
-        topicService.delete({
-          id: params.topic?.id,
+        chatService.delete({
+          id: params.chat?.id,
         })
       ).to.eventually.be.fulfilled;
     });
     it('should return 200', async () => {
       await supertest(server.httpServer)
-        .delete(`/api/topics/${params.topic?.id}`)
+        .delete(`/api/chats/${params.chat?.id}`)
         .expect(200);
     });
   });
