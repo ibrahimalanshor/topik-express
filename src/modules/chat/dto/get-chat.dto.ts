e74acb6b76f29526a5e13e2ca879fac6254418d7
ibra <ibrahimalanshor6@gmail.com>
2023-05-12 16:51:38 +0800

    feat: get chat topic id query required

--- src/modules/chat/dto/get-chat.dto.ts
+++ src/modules/chat/dto/get-chat.dto.ts
@@ -1,11 +1,12 @@
 import { Expose } from 'class-transformer';
-import { IsInt, IsOptional } from 'class-validator';
+import { IsInt, IsDefined, IsNotEmpty } from 'class-validator';
 import { BaseFilterDto } from '../../../common/dto/base/base-filter.dto';
 import { RowId } from '../../../common/model/model';
 
 export class GetChatDto extends BaseFilterDto {
   @Expose()
-  @IsOptional()
+  @IsDefined()
   @IsInt()
+  @IsNotEmpty()
   topic_id: RowId;
 }
